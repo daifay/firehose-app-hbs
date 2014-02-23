922dfb9e77f66deff0d4507b1f1a6555867bcace
daifay <dai.fay@gmail.com>
2014-02-23 20:05:15 +0000

    Using Amazon S3 typo fixed[

--- config/initializers/CarrierWave.rb
+++ config/initializers/CarrierWave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :aws_access_key_id      => ENV('AWS_ACCESS_KEY'),                        # required
-      :aws_secret_access_key  => ENV('AWS_SECRET_KEY')                        # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_KEY']                        # required
    #   :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
    #   :host                   => 's3.example.com',             # optional, defaults to nil
    #   :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
